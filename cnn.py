7d14e4772eab76a10b466eaf87769fac99f6e1f1
aaronalcuitasusjr <aaronbenjmin.alcuitas.20@usjr.edu.ph>
2022-12-15 13:38:54 +0800

    added prediction given an image

--- cnn.py
+++ cnn.py
@@ -1,22 +1,40 @@
+import numpy as np
+# import tkinter as tk 
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
+import keras.utils as image
+# from tkinter import filedialog 
 
-classifier = Sequential()
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Convolution2D(32, 3, 3, activation='relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Flatten())
-classifier.add(Dense(128, activation = 'relu'))
-classifier.add(Dense(1, activation = 'sigmoid'))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+cnn = Sequential()
+cnn.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation='relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+cnn.add(Convolution2D(32, 3, 3, activation='relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+cnn.add(Flatten())
+cnn.add(Dense(128, activation = 'relu'))
+cnn.add(Dense(1, activation = 'sigmoid'))
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64,64), batch_size=32, class_mode='binary')
 test_set = test_datagen.flow_from_directory('dataset/test_set', target_size=(64,64), batch_size=32, class_mode='binary')
-classifier.fit_generator(training_set, steps_per_epoch=25, epochs=10, validation_data=test_set, validation_steps=2000)
+cnn.fit(x = training_set, validation_data = test_set, epochs = 10)
+
+# root = tk.Tk() 
+# root.withdraw() 
+# file_path = filedialog.askopenfilename() 
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+print(prediction)
