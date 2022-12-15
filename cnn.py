f6545d768f0221a8e9b5b478d3cfbbc66997c291
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2022-12-15 16:14:49 +0800

    implemented new datasets and digits

--- cnn.py
+++ cnn.py
@@ -1,40 +1,55 @@
-import numpy as np
-# import tkinter as tk 
+# keras imports for the dataset and building our neural network
+from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
+from keras.utils import np_utils
+# to calculate accuracy
+from sklearn.metrics import accuracy_score
 from keras.preprocessing.image import ImageDataGenerator
 import keras.utils as image
-# from tkinter import filedialog 
+import numpy as np
 
-cnn = Sequential()
-cnn.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation='relu'))
-cnn.add(MaxPooling2D(pool_size = (2, 2)))
-cnn.add(Convolution2D(32, 3, 3, activation='relu'))
-cnn.add(MaxPooling2D(pool_size = (2, 2)))
-cnn.add(Flatten())
-cnn.add(Dense(128, activation = 'relu'))
-cnn.add(Dense(1, activation = 'sigmoid'))
-cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# loading the dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
-training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64,64), batch_size=32, class_mode='binary')
-test_set = test_datagen.flow_from_directory('dataset/test_set', target_size=(64,64), batch_size=32, class_mode='binary')
-cnn.fit(x = training_set, validation_data = test_set, epochs = 10)
+# building the input vector from the 28x28 pixels
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
 
-# root = tk.Tk() 
-# root.withdraw() 
-# file_path = filedialog.askopenfilename() 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+# normalizing the data to help with the training
+X_train /= 255
+X_test /= 255
+
+# one-hot encoding using keras' numpy-related utilities
+n_classes = 10
+print("Shape before one-hot encoding: ", y_train.shape)
+Y_train = np_utils.to_categorical(y_train, n_classes)
+Y_test = np_utils.to_categorical(y_test, n_classes)
+print("Shape after one-hot encoding: ", Y_train.shape)
+
+# building a linear stack of layers with the sequential model
+model = Sequential()
+# convolutional layer
+model.add(Conv2D(25, kernel_size=(3,3), strides=(1,1), padding='valid', activation='relu', input_shape=(28,28,1)))
+model.add(MaxPool2D(pool_size=(1,1)))
+# flatten output of conv
+model.add(Flatten())
+# hidden layer
+model.add(Dense(100, activation='relu'))
+# output layer
+model.add(Dense(10, activation='softmax'))
+
+# compiling the sequential model
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+# training the model for 10 epochs
+model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=(X_test, Y_test))
+
+test_image = image.load_img('dataset/single_prediction/seven.png', target_size = (28, 28), color_mode = "grayscale", keep_aspect_ratio = True)
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-result = cnn.predict(test_image)
-training_set.class_indices
-if result[0][0] == 1:
-    prediction = 'dog'
-else:
-    prediction = 'cat'
-print(prediction)
+result = model.predict(test_image)
+# training_set.class_indices
+print(result)
